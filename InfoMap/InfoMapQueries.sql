593426eeeaddc279779cd212cd057a8c2e4ff47d
jortiz16 <jortiz16@uw.edu>
2013-12-08 16:34:31 -0800

    Update InfoMapQueries.sql

--- InfoMap/InfoMapQueries.sql
+++ InfoMap/InfoMapQueries.sql
@@ -1,3 +1,6 @@
+--QUERIES FOR INFOMAP DATA
+
+--get top conferences
 create table topX as (
 select a.area, a.booktitle 
 from conferences a 
@@ -7,13 +10,7 @@ where booktitle in
  where i.booktitle=c.booktitle and c.area=a.area 
  group by i.booktitle order by count(*) desc limit 3) order by a.area);
 
- --BUILD MINE FROM THE NUMBER OF PUBLICATIONS
 
-
-select c.area, count(distinct(i.pubid)) from inproceedings i, topX c where i.booktitle=c.booktitle
-group by c.area;
-
-drop table topPub7500
 
 --get 7500 random publications per area
 create temporary table topPub7500 as
@@ -24,7 +21,6 @@ from inproceedings i, topX c where i.booktitle = c.booktitle
 order by c.area asc ) x
 where x.rn <= 7500
 
-drop table topAuthors7500;
 
 create temporary table topAuthors7500 as
 select a.authorid, au.pubid, b.area, count(*)
@@ -33,15 +29,6 @@ where b.pubid = pub.pubid and a.authorid = au.authorid and au.pubid = pub.pubid
 group by a.authorid, b.area, au.pubid
 order by b.area, count(*) desc
 
---check the count of total authors
-select count(*) from topAuthors7500 a
---101390 total authors
-
-drop table final;
---final table (can be random, but eliminate for now) -- ready to cluster
-
-select distinct p.pubid, pub.title from topPub7500 p, publication pub where p.pubid = pub.pubid and pub.title not like '%"%';
-
 
 --find the truth for each author -- dump to truth.csv
 select temp.authorid, temp.name, temp.area, temp.count, (temp.count)/sum(temp.count) over (partition by temp.authorid) percent2 from (
@@ -49,186 +36,191 @@ select a.authorid, au.name, replace(replace(au.name, ' ', '_'), '.', '') name2, 
  from topAuthors7500 as a, author as au, inproceedings as i, topX as c where au.authorid = a.authorid and a.pubid = i.pubid and i.booktitle = c.booktitle group by a.authorid, au.name, c.area order by c.area) as temp
 where percent > 0.05;
 
---After clustering, tying results to the author
 
-
-
---query to dump csv to table
-create table clusteringResults 
-(pubid int, cluster char(20));
-
-COPY clusteringResults FROM 'C:/Users/Public/results3ID.csv' DELIMITER ',' CSV;
-
-select count(*) from clusteringResults
-
-drop table countsArea
---FROM WEKA
-create temporary table countsArea as
-SELECT  r.cluster, b.area, count(*)
- FROM clusteringResults r, 
-      topPub7500 b
- WHERE r.pubid = b.pubid
-Group by cluster, area
-order by cluster asc, count(*) desc
-
-
-select * from sumsCluster a limit 100
-
-drop table sumsCluster
---For each cluster, count how many publications total
-create temporary table sumsCluster as
- select cluster, sum(count)
- FROM countsArea
- group by cluster
-
- drop table percentages
-
--- Get the percentages of each cluster (each cluster will have several areas and percentages)
-create temporary table percentages as
-select a.cluster, a.area, a.count/c.sum * 100 as percent
- from countsArea a, sumsCluster c
- where a.cluster = c.cluster
- order by cluster asc, percent  desc
-
- drop table maxCluster
---find the cluster areas (based on max)
-create temporary table maxCluster as
-select x.cluster, p.area
-from
-(select a.cluster , max(percent) as maxP
-from percentages a
-group by a.cluster) x, percentages p 
-where p.cluster = x.cluster and p.percent = x.maxP
-
-select * from maxCluster
-
- drop table authorClusterCounts
-----for each author, find the area and num publications
-create temporary table authorClusterCounts as
-select a.authorid, c.area, count(*)
-from topAuthors7500 a, authored au, topPub7500 r, maxCluster c,
-     results res
-where a.authorid = au.authorid and au.pubid = r.pubid and c.cluster = res.cluster 
-and res.pubid = r.pubid
-group by a.authorid, c.area
-order by a.authorid, c.area
-
- drop table countTotalArticles
---for each author find the total number of publications
-create temporary table countTotalArticles as
-select a.authorid, sum(a.count)
-from authorClusterCounts a
-group by a.authorid
-order by a.authorid
-
-drop table mine 
-create temporary table mine as
-select distinct a.authorid, c.area, c.count, c.count/a.sum * 100 as percent
-from countTotalArticles a, authorClusterCounts c
-where a.authorid = c.authorid
-
---To csv for evaluation
-select * from mine
-
---dumping mine to csv 
-
-
---clean up gibhub
---side note: Also turn in the code used to compare these two csv files 
-
-
---version 2 
-drop table authorsExtended
---authors from topX and find their coauthors (don't have to be from the conferences -- but does truth have them?)
---getting more outside publications? (outside the 7500)
-create temporary table authorsExtended as
-select distinct au2.authorid
-from topAuthors7500 t, authored au, topX pub, inproceedings i, authored au2
-Where t.authorid = au.authorid 
-and au.pubid = i.pubid 
-and i.booktitle = pub.booktitle
-and au2.pubid = i.pubid 
-and au.authorid != au2.authorid
-
---get all new authors -- more authors to fit to the clusters generated earlier!
-drop table allAuthors
-create temporary table allAuthors as 
-select distinct *
-from(
-select authorid from authorsExtended union (select distinct authorid from topAuthors7500)) x
-
---checking if there really are more authors
-select distinct from allAuthors;
-select distinct(authorid) from topAuthors7500;
-
---get beginning stats of all the new authors
-drop table authorClusterCounts
-create temporary table authorClusterCounts as
-select a.authorid, c.area, count(*)
-from allAuthors a, authored au, inproceedings r, maxCluster c,
-     results res
-where a.authorid = au.authorid and au.pubid = r.pubid and c.cluster = res.cluster 
-and res.pubid = r.pubid
-group by a.authorid, c.area
-order by a.authorid, c.area
-
- drop table countTotalArticles
---for each author find the total number of publications
-create temporary table countTotalArticles as
-select a.authorid, sum(a.count)
-from authorClusterCounts a
-group by a.authorid
-order by a.authorid
-
-drop table mine 
-create temporary table mine as
-select distinct a.authorid, c.area, c.count, c.count/a.sum * 100 as percent
-from countTotalArticles a, authorClusterCounts c
-where a.authorid = c.authorid
-
---To csv for evaluation
-select * from mine
-
-select count(*) from topAuthors7500
-
-drop table sampleAuthors
-drop table sampleAuthors
-create temporary table sampleAuthors as 
-select * from topAuthors7500 order by random()*197547 LIMIT 100000
-
-drop table coauthorsLarge
---INFO MAP -- co-authors (there weren't too many connected before) -- trying this now
-create temporary table coauthorsLarge as 
+--COAUTHORS FOR INFOMAP
+create temporary table level1 as
 select distinct au.authorid as author, au2.authorid as coauthor
-from topAuthors7500 au, topX pub, inproceedings i, topAuthors7500 au2
-Where  au.pubid = i.pubid 
-and i.booktitle = pub.booktitle
-and au2.pubid = i.pubid 
-and au.authorid != au2.authorid
+FROM  topAuthors7500 au,  topAuthors7500 au2
+where au.authorid != au2.authorid and au.pubid = au2.pubid
 
- 
-select distinct c.author as c1author, c.coauthor as c1coauthor
-from coauthorsLarge c, coauthorsLarge c2, coauthorsLarge c3
-where c.coauthor = c2.author and c2.coauthor = c3.author
-and c.author != c3.author
+--recursive queries
+select distinct author from level1
+
+create temporary table level2 as
+select distinct l.author, l.coauthor
+from level1 l, level1 l2
+where l.coauthor = l2.author
+and l.author != l2.coauthor
+
+create temporary table level3 as
+select distinct l.author, l.coauthor
+from level2 l, level2 l2
+where l.coauthor = l2.author
+and l.author != l2.coauthor
+
+create temporary table level2 as
+select distinct l.author, l.coauthor
+from level3 l, level3 l2
+where l.coauthor = l2.author
+and l.author != l2.coauthor
+
+select distinct author from level1
+select distinct author from level2
+
+create temporary table counts as 
+select au1.authorid as author, au2.authorid as coauthor, count(*) as count
+from topAuthors7500 au1, topAuthors7500 au2
+where au1.authorid != au2.authorid and au1.pubid = au2.pubid
+group by au1.authorid, au2.authorid
+
+--weighted version
+select distinct l.author, l.coauthor, c.count
+from level3 l, level3 l2, counts c
+where l.coauthor = l2.author
+and l.author != l2.coauthor and c.author = l.author and c.coauthor = l.coauthor
 
 
-select * from topAuthors7500 c where authorid=1250653;
+--from INFOMAP after parsing the map file
+drop table clusterPubUnweighted
+create table clusterPubUnweighted (
+cluster integer,
+authorid integer
+);
+
+copy clusterPubUnweighted FROM 'C:/Users/Public/outputMAP.txt' DELIMITER ',' CSV;
 
 
-SELECT * FROM AUTHOR WHERE AUTHORID = 1250653
+drop table countAreasWithout
+create temporary table countAreasWithout as
+select c.cluster, con.area, count(*) as count
+from clusterPubUnweighted c, authored au, publication pub, inproceedings i, conferences con
+where c.authorid = au.authorid and au.pubid = pub.pubid and pub.pubid = i.pubid and i.booktitle = con.booktitle
+group by c.cluster, con.area
+order by c.cluster asc, count desc
+
+create temporary table intermed as
+select distinct c.cluster, con.area, pub.pubid
+from clusterPubUnweighted c, authored au, publication pub, inproceedings i, conferences con
+where c.authorid = au.authorid and au.pubid = pub.pubid and pub.pubid = i.pubid and i.booktitle = con.booktitle
+
+drop table intermed
+
+drop table countsCluster
+create temporary table countsCluster as
+select i.cluster, i.area, count(*) as count
+from intermed i
+group by i.cluster, i.area
+order by i.cluster asc, count(*) desc
+
+ drop table totalCountClusters
+--for each author find the total number of publications
+create temporary table totalCountClusters as
+select i.cluster, sum(i.count)
+from countsCluster i
+group by i.cluster
 
 
---WEIGHTED GRAPH of AUTHORS/COAUTHORS
-create temporary table coauthorsLarge as 
-select  au.authorid as author, au2.authorid as coauthor, count(*) as weight
-from topAuthors7500 au, topX pub, inproceedings i, topAuthors7500 au2
-Where  au.pubid = i.pubid 
-and i.booktitle = pub.booktitle
-and au2.pubid = i.pubid 
-and au.authorid != au2.authorid
-group by au.authorid, au2.authorid
+drop table labelCluster 
+create temporary table labelCluster as
+select distinct a.cluster, a.area, a.count/c.sum * 100 as percent
+from countsCluster a, totalCountClusters c
+where a.cluster = c.cluster
 
+drop table authorCluster
+select a.authorid, l.area, l.percent
+from labelCluster l, clusterPubUnweighted a
+where l.cluster = a.cluster and percent > .2
+order by a.authorid
+
+
+select temp.authorid, temp.area, (temp.count)/sum(temp.count) over (partition by temp.authorid) percent2 
+from (
+select au.authorid, a.area, count(*), (count(*))/sum(count(*)) over (partition by au.authorid) percent
+ from countsCluster a, totalCountClusters c, clusterPubUnweighted au
+ where a.cluster = c.cluster and c.cluster = au.cluster
+ group by au.authorid, a.area order by authorid) as temp
+where percent > 0.2
+order by authorid
+
+
+select temp.authorid, temp.area, temp.count, (temp.count)/sum(temp.count) over (partition by temp.authorid) percent2 
+from (
+select au.authorid, c.area, count(*) as count, (count(*))/sum(count(*)) over (partition by au.authorid) percent
+ from topAuthors7500 as au, inproceedings as i, topX as c 
+ where au.pubid = i.pubid and i.booktitle = c.booktitle
+ group by au.authorid, c.area order by c.area) as temp
+where percent > 0.2;
+
+
+
+--WEIGHTED HERE
+drop table clusterPubWeighted
+create table clusterPubWeighted (
+cluster integer,
+authorid integer
+);
+
+copy clusterPubWeighted FROM 'C:/Users/Public/outputMAP.txt' DELIMITER ',' CSV;
+
+
+select distinct authorid from clusterPubWeighted
+
+
+drop table countAreasWithout
+create temporary table countAreasWithout as
+select c.cluster, con.area, count(*) as count
+from clusterPubWeighted c, authored au, publication pub, inproceedings i, conferences con
+where c.authorid = au.authorid and au.pubid = pub.pubid and pub.pubid = i.pubid and i.booktitle = con.booktitle
+group by c.cluster, con.area
+order by c.cluster asc, count desc
+
+
+create temporary table intermed as
+select distinct c.cluster, con.area, pub.pubid
+from clusterPubWeighted c, authored au, publication pub, inproceedings i, conferences con
+where c.authorid = au.authorid and au.pubid = pub.pubid and pub.pubid = i.pubid and i.booktitle = con.booktitle
+
+drop table intermed
+
+drop table countsCluster
+create temporary table countsCluster as
+select i.cluster, i.area, count(*) as count
+from intermed i
+group by i.cluster, i.area
+order by i.cluster asc, count(*) desc
+
+ drop table totalCountClusters
+--for each author find the total number of publications
+create temporary table totalCountClusters as
+select i.cluster, sum(i.count)
+from countsCluster i
+group by i.cluster
+
+
+drop table labelCluster 
+create temporary table labelCluster as
+select distinct a.cluster, a.area, a.count/c.sum * 100 as percent
+from countsCluster a, totalCountClusters c
+where a.cluster = c.cluster
+
+drop table authorCluster
+create temporary table authorCluster as
+select a.authorid, l.area, l.percent
+from labelCluster l, clusterPubUnweighted a
+where l.cluster = a.cluster and percent > .2
+order by a.authorid
+
+
+select distinct authorid from authorCluster
+
+select temp.authorid, temp.area, (temp.count)/sum(temp.count) over (partition by temp.authorid) percent2 
+from (
+select au.authorid, a.area, count(*), (count(*))/sum(count(*)) over (partition by au.authorid) percent
+ from countsCluster a, totalCountClusters c, clusterPubUnweighted au
+ where a.cluster = c.cluster and c.cluster = au.cluster
+ group by au.authorid, a.area order by authorid) as temp
+where percent > 0.2
+order by authorid
 
 
 
